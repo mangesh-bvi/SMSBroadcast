ff11ed91ab25aa93b78c254004c44f4927db27a5
hitesh.gopal@brainvire.com <hitesh.gopal@brainvire.com>
2020-06-05 16:02:45 +0530

    All New Changes

---
+++ SMSBroadcastHomeshop/Model/SMSSendResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMSBroadcastHomeshop.Model
+{
+    public class SMSSendResponse
+    {
+        public string GUID { get; set; }
+        public string SubmitDate { get; set; }
+        public string ID { get; set; }
+        public string ErrorSEQ { get; set; }
+        public string ErrorCODE { get; set; }
+    }
+}
